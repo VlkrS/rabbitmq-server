2c0b4edad07c61d6687422e231a054d3dd280dd5
Luke Bakken <luke@bakken.io>
2022-09-01 16:07:22 -0700

    Reformat à la @lukebakken

--- deps/amqp_client/src/amqp_channel_sup.erl
+++ deps/amqp_client/src/amqp_channel_sup.erl
@@ -24,18 +24,12 @@ start_link(Type, Connection, ConnName, InfraArgs, ChNumber,
     Identity = {ConnName, ChNumber},
     {ok, Sup} = supervisor:start_link(?MODULE, [Consumer, Identity]),
     [{gen_consumer, ConsumerPid, _, _}] = supervisor:which_children(Sup),
-    {ok, ChPid} = supervisor:start_child(
-        Sup, #{
-            id => channel,
-            start =>
-                {amqp_channel, start_link, [Type, Connection, ChNumber, ConsumerPid, Identity]},
-            restart => transient,
-            significant => true,
-            shutdown => ?WORKER_WAIT,
-            type => worker,
-            modules => [amqp_channel]
-        }
-    ),
+    StartMFA = {amqp_channel, start_link, [Type, Connection, ChNumber, ConsumerPid, Identity]},
+    ChildSpec = #{id => channel, start => StartMFA,
+                  restart => transient, significant => true,
+                  shutdown => ?WORKER_WAIT, type => worker,
+                  modules => [amqp_channel]},
+    {ok, ChPid} = supervisor:start_child(Sup, ChildSpec),
     case start_writer(Sup, Type, InfraArgs, ConnName, ChNumber, ChPid) of
         {ok, Writer} ->
             amqp_channel:set_writer(ChPid, Writer),
@@ -66,28 +60,14 @@ start_writer(_Sup, direct, [ConnPid, Node, User, VHost, Collector, AmqpParams],
     end;
 start_writer(Sup, network, [Sock, FrameMax], ConnName, ChNumber, ChPid) ->
     GCThreshold = application:get_env(amqp_client, writer_gc_threshold, ?DEFAULT_GC_THRESHOLD),
-    supervisor:start_child(
-        Sup,
-        #{
-            id => writer,
-            start =>
-                {rabbit_writer, start_link, [
-                    Sock,
-                    ChNumber,
-                    FrameMax,
-                    ?PROTOCOL,
-                    ChPid,
-                    {ConnName, ChNumber},
-                    false,
-                    GCThreshold
-                ]},
-            restart => transient,
-            significant => true,
-            shutdown => ?WORKER_WAIT,
-            type => worker,
-            modules => [rabbit_writer]
-        }
-    ).
+    StartMFA = {rabbit_writer, start_link,
+                [Sock, ChNumber, FrameMax, ?PROTOCOL, ChPid,
+                 {ConnName, ChNumber}, false, GCThreshold]},
+    ChildSpec = #{id => writer, start => StartMFA,
+                  restart => transient, significant => true,
+                  shutdown => ?WORKER_WAIT, type => worker,
+                  modules => [rabbit_writer]},
+    supervisor:start_child(Sup, ChildSpec).
 
 init_command_assembler(direct)  -> {ok, none};
 init_command_assembler(network) -> rabbit_command_assembler:init(?PROTOCOL).
